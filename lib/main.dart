0529049fe4628a774015c734a3c8c643eabbea63
Ren Amamiyaa <ren.amamiya.com@gmail.com>
2021-08-12 09:36:54 -0300

    Agenda de contatos

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:agenda_de_contatos/screens/home/home.dart';
+import 'package:agenda_de_contatos/style.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+      theme: ThemeData(
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: redTheme,
+        ),
+        dividerTheme: DividerThemeData(
+          color: grayDivider,
+          thickness: 1,
+        ),
+        iconTheme: IconThemeData(
+          color: blueTheme,
+          size: 20,
+          ),
+        primarySwatch: blueTheme,
+      ),
+    )
+  );
+}
---
+++ lib/provider/list_of_contacts.dart
@@ -0,0 +1,65 @@
+import 'package:agenda_de_contatos/models/contact.dart';
+
+List <Contact> listOfContacts = [
+//Nicole
+Contact(
+  name: "Nicole Costa",
+  photo: "assets/assets/images/Nicole.jpg",
+  email: "nicole.costa@gmail.com",
+  phone:"+55 51 11111-1111",  
+),
+//Camila
+Contact(
+  name: "Camila Santos",
+  photo: "assets/assets/images/Camila.jpg",
+  email: "camila.santosgmail.com",
+  phone:"+55 51 11111-1111",  
+  isFavorite: true,
+),
+//Claudia
+Contact(
+  name: "Claudia de Paula",
+  photo: "assets/assets/images/Claudia.jpg",
+  email: "claudia.paula@gmail.com",
+  phone:"+55 51 11111-1111",  
+),
+//Juliana
+Contact(
+  name: "Juliana Oliveira",
+  photo: "assets/assets/images/Juliana.jpg",
+  email: "juliana.oliv@gmail.com",
+  phone:"+55 51 11111-1111",  
+),
+//Lucas
+Contact(
+  name: "Lucas Araujo",
+  photo: "assets/assets/images/Lucas.jpg",
+  email: "lucas.araujo@gmail.com",
+  phone:"+55 51 11111-1111",  
+  isFavorite: true,
+
+),
+//Marina
+Contact(
+  name: "Marina Silveira",
+  photo: "assets/assets/images/Marina.jpg",
+  email: "marina.silveira@gmail.com",
+  phone:"+55 51 11111-1111",  
+),
+//Paulo
+Contact(
+  name: "Paulo Nunes",
+  photo: "assets/assets/images/Paulo.jpg",
+  email: "paulo.nunes@gmail.com",
+  phone:"+55 51 11111-1111",  
+),
+//Thomas
+Contact(
+  name: "Thomas pereira",
+  photo: "assets/assets/images/Thomas.jpg",
+  email: "thomas.pereira@gmail.com",
+  phone:"+55 51 11111-1111",  
+  isFavorite: true,  
+),
+];
+
